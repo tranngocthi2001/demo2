fd8bba902065590ee438f6ac8275cced346f9a4f
thi <thi@gmail.com>
2022-10-28 13:50:51 +0700

    add Source.cpp

--- Source.cpp
+++ Source.cpp
@@ -10,6 +10,13 @@ int main()
 	int a, b;
 	cout << "CHUONG TRINH TINH TOAN CO BAN" << endl;
 	//Cac ban code tai day
+	int cong(int a, int b);//SV A
+	{
+		int tong=0;
+		tong=a+b;
+		return tong;
+	}
+cong(a, b);
 
 	cout << endl;
 	system("pause");
