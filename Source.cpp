b628052a8999cdcc526ddc8e14d8a694649a4d9a
luan <luan@gmail.com>
2022-10-28 14:11:53 +0700

    addcpp

--- Source.cpp
+++ Source.cpp
@@ -10,7 +10,12 @@ int main()
 	int a, b;
 	cout << "CHUONG TRINH TINH TOAN CO BAN" << endl;
 	//Cac ban code tai day
-
+	int nhan(int a,int b){
+	int tich=0;
+	tich =a*b;
+	return tich;
+	}
+	nhan(a,b);
 	cout << endl;
 	system("pause");
 	return 0;
