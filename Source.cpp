e0505fcc657c67f85dd753e652a79b521c3f555f
thi <thi@gmail.com>
2022-10-28 13:36:05 +0700

    add file source.cpp

---
+++ Source.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+
+using namespace std;
+int cong(int a, int b);//SV A
+int tru(int a, int b);//SV B
+int nhan(int a, int b);//SV C
+int chia(int a, int b);//SV D
+int main()
+{
+	int a, b;
+	cout << "CHUONG TRINH TINH TOAN CO BAN" << endl;
+	//Cac ban code tai day
+
+	cout << endl;
+	system("pause");
+	return 0;
+}
