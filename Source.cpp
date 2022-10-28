8d48bfd6ba4b02935db94192b77ca4dcdfabf6e1
ritran11111 <ritrandinh1999@gmail.com>
2022-10-28 14:14:11 +0700

    abc

--- Source.cpp
+++ Source.cpp
@@ -8,10 +8,19 @@ int chia(int a, int b);//SV D
 int main()
 {
 	int a, b;
+	
 	cout << "CHUONG TRINH TINH TOAN CO BAN" << endl;
 	//Cac ban code tai day
+	int tru(int a, int b)
+	{
+	int tru=0;
+	tru = (a-b)
+	return tru;
+	}
 
+	
 	cout << endl;
+	cout<<"hieu 2 so ab la:"<<tru(a,b)
 	system("pause");
 	return 0;
 }
